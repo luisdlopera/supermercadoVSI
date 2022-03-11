a6befd9b52f18afbc9a8065d1485f3b2b34ec280
Luis David Lopera <luis.lopera28@gmail.com>
2022-03-11 16:45:45 -0500

    Primer commit

---
+++ Supermercado/Supermercado/ComposedProduct.cs
@@ -0,0 +1,9 @@
+﻿using Supermercado;
+
+internal class ComposedProduct : Product
+{
+    public string Description { get; set; }
+    public float Discount { get; set; }
+    public int Id { get; set; }
+    public List<Product> Products { get; set; }
+}
